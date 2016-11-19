6064a11043406faa6771cdd2c4bc64b2b36449df
青春呐 <wttzero@foxmail.com>
2016-11-19 17:17:11 +0800

    2016 11 19  移动 Canvas 移动内容

--- app/src/main/java/com/zero/simple/view/TView.java
+++ app/src/main/java/com/zero/simple/view/TView.java
@@ -8,9 +8,11 @@ import android.graphics.Paint;
 import android.graphics.RectF;
 import android.os.Build;
 import android.util.AttributeSet;
+import android.view.MotionEvent;
 import android.view.View;
 
 import com.zero.library.utils.DensityUtils;
+import com.zero.library.utils.KLog;
 
 
 /**
@@ -20,6 +22,14 @@ import com.zero.library.utils.DensityUtils;
 
 public class TView extends View {
     private Paint mPaint;
+    /**
+     * 边框颜色
+     */
+    private int frameColor = Color.RED;
+    /**
+     * 内容颜色
+     */
+    private int contentColor = Color.BLACK;
 
     public TView(Context context) {
         super(context);
@@ -72,18 +82,25 @@ public class TView extends View {
         }
     }
 
+    float widthX = -1;
+    float heightY = -1;
+
     @Override
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
-        int height = getHeight();
-        int width = getWidth();
+        if (widthX < 0) {
+            widthX = getWidth() / 2;
+        }
+        if (heightY < 0) {
+            heightY = getHeight() / 2;
+        }
 //        两个矩形s实现边框
 //
 //        //矩形 外
-//        mPaint.setColor(Color.BLACK);
+//        mPaint.setColor(contentColor);
         RectF rectF = new RectF();
-        rectF.left = (width - DensityUtils.dp2px(getContext(), 60)) / 2;
-        rectF.top = (height - DensityUtils.dp2px(getContext(), 50)) / 2;
+        rectF.left = widthX - DensityUtils.dp2px(getContext(), 60) / 2;
+        rectF.top = heightY - DensityUtils.dp2px(getContext(), 50) / 2;
         rectF.right = rectF.left + DensityUtils.dp2px(getContext(), 60);
         rectF.bottom = rectF.top + DensityUtils.dp2px(getContext(), 50);
         float strokeWidth = DensityUtils.dp2px(getContext(), 2);
@@ -101,33 +118,34 @@ public class TView extends View {
 //        mPaint.reset();
 
         //直线 A
-        mPaint.setColor(Color.RED);
+        mPaint.setColor(frameColor);
         mPaint.setStrokeWidth(strokeWidth);
         canvas.drawLine(rectF.left, rectF.top, rectF.right, rectF.bottom, mPaint);
         mPaint.reset();
 
         //直线 B
-        mPaint.setColor(Color.RED);
+        mPaint.setColor(frameColor);
         mPaint.setStrokeWidth(strokeWidth);
         canvas.drawLine(rectF.left, rectF.bottom, rectF.right, rectF.top, mPaint);
         mPaint.reset();
-        //带边框的矩形
-        mPaint.setColor(Color.BLACK);
+        //带边框的矩形+++++++
+        mPaint.setColor(contentColor);
         mPaint.setStrokeWidth(strokeWidth);
-        mPaint.setStyle(Paint.Style.STROKE);//设置显示样式 设置为中空
+        mPaint.setStyle(Paint.Style.STROKE);//设置显示样
+        // 式 设置为中空
         canvas.drawRect(rectF, mPaint);
         mPaint.reset();
 
         //绘制圆形
-        mPaint.setColor(Color.RED);
+        mPaint.setColor(frameColor);
         mPaint.setStyle(Paint.Style.STROKE);//设置为中空
         mPaint.setStrokeWidth(strokeWidth);
         mPaint.setAntiAlias(true);//设置抗锯齿
-        canvas.drawCircle(width / 2, height / 2, DensityUtils.dp2px(getContext(), 15), mPaint);
+        canvas.drawCircle(widthX, heightY, DensityUtils.dp2px(getContext(), 15), mPaint);
         mPaint.reset();
 
         //绘画椭圆
-        mPaint.setColor(Color.RED);
+        mPaint.setColor(frameColor);
         mPaint.setStrokeWidth(strokeWidth);
         mPaint.setAntiAlias(true);//设置抗锯齿
         mPaint.setStyle(Paint.Style.STROKE);//设置为中空
@@ -136,10 +154,48 @@ public class TView extends View {
 
         //绘制文字
 
-        mPaint.setColor(Color.RED);
+        mPaint.setColor(frameColor);
         mPaint.setTextSize(DensityUtils.sp2px(getContext(), 15));
         mPaint.setUnderlineText(true);//是否显示下划线
-        canvas.drawText("你好啊", width / 2, rectF.bottom + mPaint.getTextSize(), mPaint);
+        canvas.drawText("你好啊", widthX, rectF.bottom + mPaint.getTextSize(), mPaint);
         mPaint.reset();
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        switch (event.getAction()) {
+            case MotionEvent.ACTION_DOWN:
+//                KLog.i("-----MotionEvent.ACTION_DOWN");
+                frameColor = (frameColor == Color.RED ? Color.BLACK : Color.RED);
+                contentColor = (contentColor == Color.RED ? Color.BLACK : Color.RED);
+                invalidate();
+                break;
+            case MotionEvent.ACTION_MOVE:
+//                KLog.i("-----MotionEvent.ACTION_MOVE---Y  " + event.getY() + "  ---X  " + event.getX());
+                widthX = event.getX();
+                heightY = event.getY();
+                if (event.getX() < 0) {
+                    widthX = 0;
+                } else if (event.getX() > getWidth()) {
+                    widthX = getWidth();
+                }
+                if (event.getY() < 0) {
+                    heightY = 0;
+                } else if (event.getY() > getHeight()) {
+                    heightY = getHeight();
+                }
+                invalidate();
+                break;
+            case MotionEvent.ACTION_UP:
+//                KLog.i("-----MotionEvent.ACTION_UP");
+                frameColor = (frameColor == Color.RED ? Color.BLACK : Color.RED);
+                contentColor = (contentColor == Color.RED ? Color.BLACK : Color.RED);
+                invalidate();
+                break;
+            default:
+                KLog.i("Action-----" + event.getAction());
+                break;
+        }
+        return true;
     }
 }
